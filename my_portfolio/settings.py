48b970bd174f3225f60b4b3b71b5e217cc41f8b8
artneto <arthur_neto@icloud.com>
2023-02-15 13:46:45 +0000

    create cache for contenxt processor

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'my_portfolio.context_processors.project_context'
 ]
 
 ROOT_URLCONF = 'my_portfolio.urls'
