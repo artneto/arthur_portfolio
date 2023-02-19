63a3094c0e269030e89f1d2f872f78eba9d7d776
artneto <arthur_neto@icloud.com>
2023-02-19 18:13:38 +0000

    create views and forms for website

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,45 @@
 from django.contrib import admin
-from models. import (
-    
+from .models import (
+    Skill,
+    UserProfile,
+    ContactProfile,
+    Testimonials,
+    Media,
+    Portfolio,
+    Blog,
+    Certificate,
 )
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user')
+
+@admin.register(ContactProfile)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'name')
+
+@admin.register(Testimonials)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_active')
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_active')
+    readonly_fields = ('slug',)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_active')
+    readonly_fields = ('slug',)
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'score')
--- base/views.py
+++ base/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import (
+        UserProfile,
+        Blog,
+        Portfolio,
+        Testimonial,
+        Certificate
+)
 
-# Create your views here.
+
+from django.views import generic
+
+
+from . forms import ContactForm
+
+class IndexView(generic.TemplateView):
+    template_name = 'base/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data( **kwargs)
+
+        testimonials = Testimonial.objects.filter(is_active=True)
+        certificates = Certificate.objects.filter(is_active=True)
+        blogs = Blog.objects.filter(is_active=True)
+        portfolio = Portfolio.objects.filter(is_active=True)
+
+        context['testimonials'] = testimonials
+        context['certificates'] = certificates
+        context['blogs'] = blogs
+        context['portfolio'] = portfolio
+        return context
+
+
+class ContactView(generic.FormView):
+    template_name = 'base/contact.html'
+    form_class = ContactForm
+    sucess_url = "/"
+
+    def form_valid(self, form):
+        form.save()
+        messages.sucess(self.request, 'Thank you. We will be in touch soon!')
+        return super().form_valid(form)
+
+        
